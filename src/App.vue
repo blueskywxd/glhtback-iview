b367d700135b4b598a61c45c0e803eec340970d2
blueskywxd <781447551wxd@sina.com>
2018-03-06 10:28:21 +0800

    添加字体包，添加首页

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Header from './components/Header'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+// import 'assets/common/base.scss'
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="header">
+    <div class="head-logo l"><a class="icon-logo"></a></div>
+    <div class="head-icon l">
+      <a href="#" class="icon-gl_sosuo"></a>
+      <a href="#" class="icon-gl_gongneng"></a>
+    </div>
+    <ul class="head-nav l">
+      <li><a href="#" class="focus">系统</a><i></i></li>
+      <li><a href="#">字典</a><i></i></li>
+      <li><a href="#">链接</a><i></i></li>
+      <li><a href="#">广告</a><i></i></li>
+      <li><a href="#">资讯</a><i></i></li>
+      <li><a href="#">会员</a><i></i></li>
+      <li><a href="#">服务</a><i></i></li>
+      <li><a href="#">店铺</a><i></i></li>
+      <li><a href="#">商品</a><i></i></li>
+      <li><a href="#">活动</a><i></i></li>
+      <li><a href="#">订单</a><i></i></li>
+      <li><a href="#">财务</a><i></i></li>
+      <li><a href="#">模板</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+// @import "../assets/common/common.scss";
+@import "Header"
+</style>
